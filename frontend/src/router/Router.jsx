1e7646f8eebf57cc70f7b63ac0d1b149b4466f2e
Muhammad Bilal <mbilal45422@gmail.com>
2024-03-05 19:47:41 +0500

    setup menu page

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/home/Home";
+import Menu from "../pages/menuPage/Menu";
 
 const router = createBrowserRouter([
     {
@@ -11,7 +11,11 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home/>
-            }
+            },
+            {
+                path: '/menu',
+                element: <Menu/>
+            },
         ]
     },
 ]);
